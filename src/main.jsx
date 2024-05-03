6a1567d9bb8a5d6b32f983927dcc229bc1ac132c
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-05-03 23:19:13 +0700

    change router from browser router to Hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import HomePage from './pages/home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Stargezer } from './data/Stargezer'
 import { StargazerX } from './data/StargazerX'
 import { Creta } from './data/Creta'
@@ -10,7 +10,7 @@ import { SantaFe } from './data/SantaFe'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/stargezer' element={<Stargezer/>} />
@@ -19,6 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/santafe' element={<SantaFe/>} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
